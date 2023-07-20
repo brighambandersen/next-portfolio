e7c379bd2e69b3257db60158af92b1510cc9eec0
Brigham Andersen <brighamband@gmail.com>
2023-07-20 15:29:06 -0600

    Ignore lint errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import PageHeader from '@/app/components/PageHeader';
 import WebVideo from '@/app/components/WebVideo';
